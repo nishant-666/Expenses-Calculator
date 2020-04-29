91a4acaaa5c715499de82a5d257a2962b4e1dedf
nishant666 <nishants440@gmail.com>
2020-04-29 22:24:28 +0530

    Updates

---
+++ precache-manifest.fd8294ac48c576b8fe5675324b5d8069.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec8f42417a58c28b010152fd4483f6a",
+    "url": "/Expenses-Calculator/index.html"
+  },
+  {
+    "revision": "c6eaf7fd0afa290e38ca",
+    "url": "/Expenses-Calculator/static/css/main.1abc867d.chunk.css"
+  },
+  {
+    "revision": "ac7a5c3e32cd02e84a22",
+    "url": "/Expenses-Calculator/static/js/2.c166ff5e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Expenses-Calculator/static/js/2.c166ff5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6eaf7fd0afa290e38ca",
+    "url": "/Expenses-Calculator/static/js/main.91dfb1d8.chunk.js"
+  },
+  {
+    "revision": "ac503854212bcb81f618",
+    "url": "/Expenses-Calculator/static/js/runtime-main.465114bd.js"
+  }
+]);
