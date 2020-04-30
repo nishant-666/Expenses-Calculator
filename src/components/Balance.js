795c00125c385087ecff70046af9f641b4b2721f
nishant666 <nishants440@gmail.com>
2020-04-30 11:25:17 +0530

    v3

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -11,7 +11,7 @@ export const Balance = () => {
   return (
     <>
       <h4>Your Balance</h4>
-    <h1>₹{numberWithCommas(total)}</h1>
+    <h1>{numberWithCommas(total)}</h1>
     </>
   )
 }
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,16 +1,17 @@
+
 import React, {useContext} from 'react';
 import { GlobalContext } from '../context/GlobalState';
 import { numberWithCommas} from '../utils/format'
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
 
-  const sign = transaction.amount < 0 ? 'You Spent ' : 'You Earned ';
+  const sign = transaction.amount < 0 ? 'You Spent ' : 'You Earned';
 
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
       {transaction.text} <span>{sign}₹{numberWithCommas(Math.abs(transaction.amount))}</span>
       
-      <button onClick={() => deleteTransaction(transaction._id)} className="delete-btn">X</button>
+      <button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">X</button>
     </li>
   )
 }
